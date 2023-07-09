a2ca1ce338b646bdb64ba7dd067a297fa942e140
Bruno Andrade <bruno@artsman.dev>
2023-07-09 21:58:31 +0100

    feat: created base project sctructure

---
+++ src/main/java/dev/artsman/flyfoods/FlyFoodsApiApplication.java
@@ -0,0 +1,11 @@
+package dev.artsman.flyfoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyFoodsApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlyFoodsApiApplication.class, args);
+	}
+}
