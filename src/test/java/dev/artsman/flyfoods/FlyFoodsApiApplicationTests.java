ddf1415f17a287e60120eac03758262bd9c6edfa
Bruno Andrade <bruno@artsman.dev>
2023-07-09 21:58:31 +0100

    feat: created base project sctructure

---
+++ src/test/java/dev/artsman/flyfoods/FlyFoodsApiApplicationTests.java
@@ -0,0 +1,10 @@
+package dev.artsman.flyfoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyFoodsApiApplicationTests {
+	@Test
+	void contextLoads() {}
+}
