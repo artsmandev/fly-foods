a48c4e7559971901cf6c2128968c66d7c395a2b6
Bruno Andrade <bruno@artsman.dev>
2023-08-02 00:30:26 +0100

    feat: upadted Kitchen and added Restaurant

--- src/main/java/dev/artsman/flyfoods/domain/restaurant/Restaurant.java
+++ src/main/java/dev/artsman/flyfoods/domain/restaurant/Restaurant.java
@@ -1,16 +1,18 @@
-package dev.artsman.flyfoods.domain.kitchen;
+package dev.artsman.flyfoods.domain.restaurant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import java.math.BigDecimal;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-class Kitchen {
+class Restaurant {
 	@Id
 	@EqualsAndHashCode.Include
 	private Long id;
 	private String name;
+	private BigDecimal deliveryFee;
 }
