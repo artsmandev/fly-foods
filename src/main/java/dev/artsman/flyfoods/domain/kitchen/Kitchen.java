6567e1a004437e9726ea99b046d9d9b90aa0b117
Bruno Andrade <bruno@artsman.dev>
2023-08-02 08:06:36 +0100

    feat: created primary key and auto-increment

--- src/main/java/dev/artsman/flyfoods/domain/kitchen/Kitchen.java
+++ src/main/java/dev/artsman/flyfoods/domain/kitchen/Kitchen.java
@@ -1,7 +1,11 @@
 package dev.artsman.flyfoods.domain.kitchen;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +14,9 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 class Kitchen {
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "kitchen_seq")
+	@Column(columnDefinition = "serial")
 	@EqualsAndHashCode.Include
 	private Long id;
 	private String name;
--- src/main/java/dev/artsman/flyfoods/domain/restaurant/Restaurant.java
+++ src/main/java/dev/artsman/flyfoods/domain/restaurant/Restaurant.java
@@ -1,7 +1,11 @@
 package dev.artsman.flyfoods.domain.restaurant;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import java.math.BigDecimal;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -11,6 +15,9 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 class Restaurant {
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "restaurant_seq")
+	@Column(columnDefinition = "serial")
 	@EqualsAndHashCode.Include
 	private Long id;
 	private String name;
