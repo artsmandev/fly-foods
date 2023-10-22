9bb9cac6a86896435e3b4e432ceb5a82633f3ef5
Bruno Andrade <bruno@artsman.dev>
2023-10-22 18:43:13 +0100

    refact: using the specific annotation for repository

--- src/main/java/dev/artsman/flyfoods/domain/cuisine/infrastructure/CuisineRepositoryImpl.java
+++ src/main/java/dev/artsman/flyfoods/domain/cuisine/infrastructure/CuisineRepositoryImpl.java
@@ -6,10 +6,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
+@Repository
 @RequiredArgsConstructor
 class CuisineRepositoryImpl implements CuisineRepository {
 	@PersistenceContext
